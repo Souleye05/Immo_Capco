893f121c630fd94dc5c7847041ce414a90f64fc3
Souleye05 <dsouleye105@gmail.com>
2025-05-13 17:49:22 +0000

    Refonte de la page d'accueil

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('/welcome');
-// });
-
-Route::redirect('/', '/admin/login');
+Route::get('/', function () {
+    return view('/welcome');
+});
 
 
+
+// Route::redirect('/', '/admin/login');
